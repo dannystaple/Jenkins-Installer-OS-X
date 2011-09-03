c5d32fdf7907ee62749adb84e5dfbae7ebf0ab4c
dannystaple <danny@orionrobots.co.uk>
2011-09-03 12:03:57 +0100

    Replace NSLog with print. Lets try get this macruby dependancy out - vanilla tools.

--- setup.rb
+++ setup.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env macruby
+#!/usr/bin/env ruby
 
 framework 'Foundation'
 
@@ -9,7 +9,7 @@ include FileUtils
 
 
 unless ENV['USER'] == 'root'
-  NSLog('You need to run this script as root.')
+  print('You need to run this script as root.')
   exit
 end
 
@@ -36,22 +36,22 @@ end
 
 
 ### Setup directories
-NSLog('Creating data and logging directories')
+print('Creating data and logging directories')
 mkdir_p [JENKINS_INSTALL_DIR, JENKINS_HOME_DIR, JENKINS_LOG_DIR]
 
 
 ### Download and install the .war file
-NSLog('Downloading the latest release of Jenkins')
+print('Downloading the latest release of Jenkins')
 jenkins_url = NSURL.alloc.initWithString JENKINS_DOWNLOAD_URL
 jenkins_war = NSMutableData.dataWithContentsOfURL jenkins_url
 raise 'Failed to download Jenkins' if jenkins_war.nil?
 
-NSLog('Installing Jenkins')
+print('Installing Jenkins')
 write_file(JENKINS_WAR_FILE) { |file| file.write String.new(jenkins_war) }
 
 
 ### Launchd setup
-NSLog('Creating launchd plist')
+print('Creating launchd plist')
 LAUNCHD_LABEL     = 'org.jenkins-ci.jenkins'
 LAUNCHD_DIRECTORY = '/Library/LaunchDaemons'
 LAUNCHD_FILE      = "#{LAUNCHD_LABEL}.plist"
@@ -71,12 +71,12 @@ LAUNCHD_SCRIPT    = {
   # @todo Maybe setup Bonjour using the Socket key
 }
 
-NSLog('Installing launchd plist')
+print('Installing launchd plist')
 write_file File.join(JENKINS_INSTALL_DIR, LAUNCHD_FILE) do |file|
   file.write LAUNCHD_SCRIPT.to_plist
 end
 
-NSLog('Starting launchd job for Jenkins')
+print('Starting launchd job for Jenkins')
 if File::exists?( LAUNCHD_PATH )
   rm LAUNCHD_PATH
 end
@@ -85,4 +85,4 @@ ln_s File.join(JENKINS_INSTALL_DIR, LAUNCHD_FILE), LAUNCHD_PATH
 `sudo launchctl load  #{LAUNCHD_PATH}`
 `sudo launchctl start #{LAUNCHD_LABEL}`
 
-NSLog('Jenkins install complete.')
+print('Jenkins install complete.')
